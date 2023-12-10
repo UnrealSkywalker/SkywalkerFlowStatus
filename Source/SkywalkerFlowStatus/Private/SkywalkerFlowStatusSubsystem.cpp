b4c73994a6f62bb5109c3fda9f0d1ba539dce3ab
Shyfan <410713847@qq.com>
2023-12-10 16:31:38 +0800

    优化

--- Source/SkywalkerFlowStatus/Private/SkywalkerFlowStatusSubsystem.cpp
+++ Source/SkywalkerFlowStatus/Private/SkywalkerFlowStatusSubsystem.cpp
@@ -1,3 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SkywalkerFlowStatusSubsystem.h"
+
+#include "Kismet/GameplayStatics.h"
+
+#pragma region FlowProcess
+
+bool USkywalkerFlowStatusSubsystem::OnSwitchLaunchProcess(TSubclassOf<ASkywalkerFlowProcess> NewFlowProcessClass)
+{
+	ASkywalkerFlowProcess* NewFlowProcess = Cast<ASkywalkerFlowProcess>(UGameplayStatics::BeginDeferredActorSpawnFromClass(this, NewFlowProcessClass, FTransform::Identity, ESpawnActorCollisionHandlingMethod::AlwaysSpawn, nullptr));
+
+	if (CurrentFlowProcess != nullptr)
+	{
+		CurrentFlowProcess->PreLeave();
+		CurrentFlowProcess->Leave();
+		CurrentFlowProcess->Destroy();
+	}
+
+
+	CurrentFlowProcess = NewFlowProcess;
+	CurrentFlowProcess->PreEnter();
+	CurrentFlowProcess->Enter();
+
+	return true;
+}
+
+#pragma endregion FlowProcess
--- Source/SkywalkerFlowStatus/Public/SkywalkerFlowStatusSubsystem.h
+++ Source/SkywalkerFlowStatus/Public/SkywalkerFlowStatusSubsystem.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/GameInstanceSubsystem.h"
+
+#include "SkywalkerFlowProcess.h"
+
 #include "SkywalkerFlowStatusSubsystem.generated.h"
 
 /**
@@ -13,4 +16,16 @@ UCLASS()
 class SKYWALKERFLOWSTATUS_API USkywalkerFlowStatusSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
+
+#pragma region FlowProcess
+
+private:
+	ASkywalkerFlowProcess*  CurrentFlowProcess = nullptr;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Skywalker|Flow Process")
+	bool OnSwitchLaunchProcess(TSubclassOf<ASkywalkerFlowProcess> NewFlowProcessClass);
+
+#pragma endregion FlowProcess
+
 };
